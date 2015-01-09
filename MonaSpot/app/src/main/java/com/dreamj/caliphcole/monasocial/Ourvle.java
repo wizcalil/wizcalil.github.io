64eb4caeb9d514523e96bf11c0c58c6f63b30720
wizcalil <colecaliph@gmail.com>
2015-01-09 02:18:26 -0500

    MonaSpot
    update name change

---
+++ MonaSpot/app/src/main/java/com/dreamj/caliphcole/monasocial/Ourvle.java
@@ -0,0 +1,55 @@
+package com.dreamj.caliphcole.monasocial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by CaliphCole on 12/30/2014.
+ */
+public class Ourvle extends Fragment {
+    public static final String ARG_CATEGORY_NUMBER = "category_number";
+
+    public Ourvle() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.webview, container, false);
+
+        WebView webView = (WebView) rootView.findViewById(R.id.webView);
+        String searchURL = "http://ourvle.mona.uwi.edu/";
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(searchURL);
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_CATEGORY_NUMBER));
+    }
+
+
+}
---
+++ MonaSpot/app/src/main/java/com/dreamj/caliphcole/monasocial/Splash.java
@@ -0,0 +1,84 @@
+package com.dreamj.caliphcole.monasocial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+/**
+ * Created by CaliphCole on 12/30/2014.
+ */
+public class Splash extends Activity {
+
+
+    Thread runTime;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+
+        final ImageView splashImageView = (ImageView) findViewById(R.id.ivSplash);
+        splashImageView.setBackgroundResource(R.drawable.drawablesplash);
+        final AnimationDrawable frameAnimation = (AnimationDrawable) splashImageView.getBackground();
+
+        splashImageView.post(new Runnable() {
+            @Override
+            public void run() {
+                frameAnimation.start();
+            }
+        });
+
+
+        runTime = new Thread() {
+            public void run() {
+                try {
+                    synchronized (this) {
+
+                        wait(5000);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent main = new Intent(
+                            "com.dreamj.caliphcole.monasocial.MainActivity");
+                    startActivity(main);
+                }
+
+            }
+        };
+        runTime.start();
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Auto-generated method stub
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(runTime){
+                runTime.notifyAll();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        //song.release();
+         finish();
+    }
+
+}
